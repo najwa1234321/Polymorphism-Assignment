cbc98e574668ecee5f799cb4095846e03cbd44f5
najwa1234321 <najwa.eylji97@gmail.com>
2025-11-16 15:34:55 -0500

    Update Employee.cs

--- Employee.cs
+++ Employee.cs
@@ -1,12 +1,13 @@
 using System;
 
-// Employee class implements the IQuitable interface
+// Employee class inherits the IQuitable interface.
 public class Employee : IQuitable
 {
+    // Properties for employee name.
     public string FirstName { get; set; }
     public string LastName { get; set; }
 
-    // Implementation of Quit() method
+    // Implementation of the Quit() method required by the interface.
     public void Quit()
     {
         Console.WriteLine($"{FirstName} {LastName} has quit the job.");

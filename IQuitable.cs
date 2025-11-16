a864fbf339c0afa50c3bffb863936f647b0c3242
najwa1234321 <najwa.eylji97@gmail.com>
2025-11-16 15:34:39 -0500

    Update IQuitable.cs

--- IQuitable.cs
+++ IQuitable.cs
@@ -1,7 +1,8 @@
 using System;
 
-// Interface containing a Quit() method
+// This interface requires any class that inherits it to implement the Quit() method.
 public interface IQuitable
 {
+    // Method that must be implemented by any class using this interface.
     void Quit();
 }
